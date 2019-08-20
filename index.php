e65aeb7a70b1eab94406b34d5cd73f8b0ac2d689
Dương Sang <Dng Sang@192.168.12.1>
2019-08-20 21:37:57 +0700

    thay doi

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
-    <a href="DatabaseIndex.php" target="_blank" >Work with database</a>
+    <a href="DatabaseIndex.php" target="_blank" >Database</a>
      <br/> 
-    <a href="ConnectToDB.php" target="_blank" >Connect database</a>
+    <a href="ConnectToDB.php" target="_blank" >View</a>
      <br/>
-     <a href="InsertData.php" target="_blank" >Insert data to the database</a>
+     <a href="InsertData.php" target="_blank" >Insert</a>
      <br/>
-     <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
+     <a href="UpdateData.php" target="_blank" >Update </a> 
     <br/>
-    <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
+    <a href="DeleteData.php" target="_blank" >Delete</a> 
      <br/>
      
 
