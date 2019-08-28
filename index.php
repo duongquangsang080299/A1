d2630eb441da039d9ae8b01d4def9cf6bfbbd23e
Dương Sang <Dng Sang@MAYTINH>
2019-08-28 18:58:15 +0700

    thay doi

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo"Hello World!";// A1?>
+echo 'Chào Mừng Các Bạn Đến Với freetuts.net'; // dòng ghi chú
+/*Hoặc dòng ghi chú*/
+?>
+<?php
+$sinhvien = ''; //đúng
+$_sinh_vien = ''; //đúng
+$sinh_vien90 = ''; //đúng
+?>
