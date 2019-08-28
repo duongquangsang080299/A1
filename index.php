5e054402dffd5836044d3f04c3a3f7fec9545d16
Dương Sang <Dng Sang@MAYTINH>
2019-08-28 19:06:39 +0700

    thay doi

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$sinhvien = ''; //đúng
-$_sinh_vien = ''; //đúng
-$sinh_vien90 = ''; //đúng
+/* Tạo một hằng số có tên là SDT và gán giá trị cho nó là 0909090909*/
+define('SDT', '0909090909');
+echo SDT; // xuất ra màn hình giá trị của hằng.
 ?>
