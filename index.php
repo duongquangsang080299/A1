ab49eaf95913d3f6dded22c1f55ee1dbb0ddf238
Dương Sang <Dng Sang@MAYTINH>
2019-08-28 19:38:07 +0700

    thay doi

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<?php
-$thap_phan = 123; // Số thập phân
-$so_am = -123; // Số âm
-$bat_phan = 0123; // số bát phân
-$thap_luc_phan = 0x1A; // và số thập lục phân
-?>
+<html>
+    <a href="DatabaseIndex.php" target="_blank" >Work with database</a>
+     <br/> 
+    <a href="ConnectToDB.php" target="_blank" >Connect database</a>
+     <br/>
+     <a href="InsertData.php" target="_blank" >Insert data to the database</a>
+     <br/>
+     <a href="UpDateData.php" target="_blank" >Update data to the database</a> 
+    <br/>
+    <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
+     <br/>
+     
+
+</body>
+
+     
+</html>
